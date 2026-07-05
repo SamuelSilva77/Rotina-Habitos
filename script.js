f9140757593f01fc377a836e4af579572588e5f5
Samuel Silva <samuelpds77@gmail.com>
2026-07-04 21:48:49 -0300

    HEADER TERMINADO

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 //npx @tailwindcss/cli -i ./style.css -o ./output.css --watch
 
+//BODY
+let body = document.querySelector("body");
+
 //ABRIR E FECHAR MENU
 
-let menuButton = document.getElementById("menu")
+let menuButton = document.getElementById("menu");
+let menu = document.getElementById("options");
+
+let imagem = ""
 
 menuButton.addEventListener("click", () => {
-    menuButton.classList.add("opacity-0")
+  menuButton.classList.add("opacity-0");
 
-    setTimeout(() => {
-        if(menuButton.src == "img/closeWhite.png"){
-            menuButton.src = "img/menuWhite.png"
-        }else{
-            menuButton.src = "img/closeWhite.png"
-        }
-        
+  setTimeout(() => {
+    if (imagem == "img/closeWhite.png") {
+      menuButton.src = "img/menuWhite.png";
+      imagem = "img/menuWhite.png";
+    } else {
+      menuButton.src = "img/closeWhite.png";
+      imagem = "img/closeWhite.png";
+    }
 
-        menuButton.classList.remove("opacity-0")
-        
-    }, 200);
-
-})
+    menuButton.classList.remove("opacity-0");
+    
+    menu.classList.toggle("opacity-0")
+    menu.classList.toggle("pointer-events-none")
+}, 200);
+});
